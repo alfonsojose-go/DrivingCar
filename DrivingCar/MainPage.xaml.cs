805080668076b8c02c3daa116a5722fae50f5af8
alfonsojose-go <alfonsojose.go@rdpolytech.ca>
2025-03-26 17:05:53 -0600

    Added interface

--- DrivingCar/MainPage.xaml.cs
+++ DrivingCar/MainPage.xaml.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.ServiceModel.Channels;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.System;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -25,6 +27,76 @@ namespace DrivingCar
         public MainPage()
         {
             this.InitializeComponent();
+
+            // Add key down event handler
+            this.KeyDown += MainPage_KeyDown;
+
+            // Ensure the page can receive keyboard focus
+            this.Loaded += (s, e) => this.Focus(FocusState.Programmatic);
+        }
+
+        private void MainPage_KeyDown(object sender, KeyRoutedEventArgs e)
+        {
+            // Check what the key is pressed
+            switch (e.Key)
+            {
+                case VirtualKey.Enter:
+                    btnStart_Click(btnStart, null);
+                    e.Handled = true;
+                    break;
+
+                case VirtualKey.Escape:
+                    btnExit_Click(btnExit, null);
+                    e.Handled = true;
+                    break;
+
+                case VirtualKey.Left:
+                    btnLeft_Click(btnLeft, null);
+                    e.Handled = true;
+                    break;
+
+                case VirtualKey.Right:
+                    btnRight_Click(btnRight, null);
+                    e.Handled = true;
+                    break;
+            }
+        }
+
+        private void btnStart_Click(object sender, RoutedEventArgs e)
+        {
+            btnStart.Content = "Playing...";
+        }
+
+        private void btnExit_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Exit();
+        }
+
+        private async void btnRight_Click(object sender, RoutedEventArgs e)
+        {
+
+            //test code. Put code here to move the car to the right
+            var dialog = new ContentDialog
+            {
+                Title = "Movement",
+                Content = "Right button clicked",
+                CloseButtonText = "OK"
+            };
+
+            await dialog.ShowAsync();
+        }
+
+        private async void btnLeft_Click(object sender, RoutedEventArgs e)
+        {
+            //test code. Put code here to move the car to the left
+            var dialog = new ContentDialog
+            {
+                Title = "Movement",
+                Content = "Left button clicked",
+                CloseButtonText = "OK"
+            };
+
+            await dialog.ShowAsync();
         }
     }
 }
